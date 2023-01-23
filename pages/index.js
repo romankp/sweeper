0ce294dcbaa55155737da6a88c3ad9515beafd22
Roman <romankp@gmail.com>
2023-01-22 20:25:09 -0500

    initial field list

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import React, { useState } from 'react';
+
+const populateField = size => {
+  let cells = [];
+  for (let i = 0; i < size; i++) {
+    cells.push(<li></li>);
+  }
+  return cells;
+};
+
 function Root() {
-  return <div>Test test.</div>;
+  const [fieldSize] = useState(4 * 4);
+
+  return (
+    <>
+      <h1>Sweeper</h1>
+      <ul>{populateField(fieldSize)}</ul>
+    </>
+  );
 }
 
 export default Root;
