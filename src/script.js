7750791e46fa40a51ae10e7a3c2378d2aac074a8
Roman <romankp@gmail.com>
2023-01-25 23:34:30 -0500

    render initial field

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const LAT_DIM = 4;
 const MINE_CHANCE = 20 / 100;
 
+const field = document.getElementsByTagName('ul')[0];
 let fieldSize = LAT_DIM ** 2;
 let fieldData = [];
 
@@ -19,6 +20,19 @@ const randomizeBool = chanceRatio => {
   return Math.random() > chanceRatio ? false : true;
 };
 
+// Initialize field data
 fieldData = initFieldData(fieldSize);
+
+// Render field
+fieldData.map(({ hidden }, i) => {
+  const cell = document.createElement('li');
+
+  cell.setAttribute('data-index', i);
+  if (hidden) {
+    cell.classList.add('hidden');
+  }
+
+  field.appendChild(cell);
+});
 
 console.log(fieldData);
